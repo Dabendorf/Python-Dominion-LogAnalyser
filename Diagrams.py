367ae8d9f1dc7d53a55628a819ad6601c5366eda
Lukas Schramm <lukas@dorpapst.de>
2021-01-13 17:13:22 +0100

    Added exile and trashing

---
+++ Diagrams.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+# Analyse von Dominion Spielen, graphisch
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from matplotlib.ticker import MaxNLocator
+
+
+# In[2]:
+
+
+df = pd.read_csv("results_cum.csv", encoding="utf-8")
+df
+
+
+# In[3]:
+
+
+players = ["DORk", "DORT", "O", "J", "A"]
+# players = ["D", "A"]
+
+# In[15]:
+
+
+fig, axes = plt.subplots(len(players), 2, figsize=(20, 7*len(players)))
+cnt = 0
+
+for player in players:
+    per_player_old = df.query("Name==@player")
+    per_player_basis = per_player_old.query("Karte in ['Kupfer', 'Silber', 'Gold', 'Platin', 'Anwesen', 'Herzogtum', 'Provinz', 'Kolonie', 'Fluch']")
+    per_player_kr = per_player_old.query("Karte not in ['Kupfer', 'Silber', 'Gold', 'Platin', 'Anwesen', 'Herzogtum', 'Provinz', 'Kolonie', 'Fluch']")
+
+    g=sns.lineplot(x='Zug',
+                y='Anzahl',
+                hue='Karte',
+                data=per_player_basis,
+                marker="o",
+                ax=axes[cnt, 0])
+
+    g.set_xlabel("Zug",fontsize=20)
+    g.set_ylabel("Anzahl",fontsize=20)
+    
+    axes[cnt-1, 0].yaxis.set_major_locator(MaxNLocator(integer=True))
+    axes[cnt-1, 0].set_title(player+" Basiskarten", fontsize=25)
+    axes[cnt-1, 0].xaxis.set_major_locator(ticker.MultipleLocator(2))
+    
+    g=sns.lineplot(x='Zug',
+                y='Anzahl',
+                hue='Karte',
+                data=per_player_kr,
+                marker="o",
+                ax=axes[cnt, 1])
+    
+    cnt += 1
+    g.set_xlabel("Zug",fontsize=20)
+    g.set_ylabel("Anzahl",fontsize=20)
+    # g.set(ylim=(1, None))
+    
+    axes[cnt-1, 1].yaxis.set_major_locator(MaxNLocator(integer=True))
+    axes[cnt-1, 1].set_title(player+" Königreichkarten", fontsize=25)
+    axes[cnt-1, 1].xaxis.set_major_locator(ticker.MultipleLocator(2))
+fig.savefig('results.svg')
+
--- LogAnalyser.py
+++ LogAnalyser.py
@@ -1,5 +1,11 @@
 import csv
+import re
 
+# https://stackoverflow.com/questions/9662346/python-code-to-remove-html-tags-from-a-string
+def cleanhtml(raw_html):
+	cleanr = re.compile('<.*?>')
+	cleantext = re.sub(cleanr, '', raw_html)
+	return cleantext
 class Player():
 	cards = dict()
 	name = ""
@@ -19,13 +25,39 @@ class Player():
 players = dict()
 
 def main():
-	with open("DominionLog.txt", 'r') as log_file:
+	# with open("DominionLog.txt", 'r') as log_file:
+	html_mode = False
+
+	if html_mode:
+		file_name = "Dominion Online.html"
+	else:
+		file_name = "log.txt"
+
+	with open(file_name, 'r') as log_file:
+		# htmlMode
+		start = False
+
 		# start configuration
 		turn = 0
 		for line in log_file:
+			if html_mode:
+				if "game-log" in line:
+					start = True
+
+				if not start:
+					continue
+
+				if "player-zones" in line:
+					break
+				line = cleanhtml(line).strip()
+
+				if line == "":
+					continue
+
+
 			if "beginnt" in line:
 				players_name = line[0:line.find(" beginnt")]
-				card_str = line[line.find("folgenden Karten: ")+len("folgenden Karten: "):len(line)-2].replace(".", "")
+				card_str = line[line.find("folgenden Karten: ")+len("folgenden Karten: "):len(line)-1].replace(".", "")
 				card_num_temp, card_name = card_str.split(" ")
 				card_num = int(card_num_temp)
 
@@ -62,6 +94,20 @@ def main():
 		
 		# Game itself
 		for line in log_file:
+			if html_mode:
+				if "game-log" in line:
+					start = True
+
+				if not start:
+					continue
+
+				if "player-zones" in line:
+					break
+				line = cleanhtml(line).strip()
+
+				if line == "":
+					continue
+
 			if line.startswith("Zug"):
 				line_split = line.split(" ")
 				turn = int(line_split[1])
@@ -71,6 +117,7 @@ def main():
 					line_str = line[:-1]
 				else:
 					line_str = line
+				line_str = line_str.replace(" auf den Nachziehstapel", "")
 				line_split = line_str.split(" ")
 				pos_nimmt = line_split.index("nimmt")
 				amount_str = line_split[pos_nimmt+1]
@@ -97,7 +144,7 @@ def main():
 
 			# trash cards
 			if "entsorgt" in line:
-				if line.endswith("."):
+				if line.strip().endswith("."):
 					line_str = line[:-1]
 				else:
 					line_str = line
@@ -108,21 +155,60 @@ def main():
 
 				# names with two parts
 				if pos_nimmt+2 < len(line_split)-1:
-					card_name = line_split[pos_nimmt+2] + " " + line_split[pos_nimmt+3].strip()
+					card_name = line_split[pos_nimmt+2] + " " + line_split[pos_nimmt+3].strip().replace(".", "")
 				else:
-					card_name = line_split[pos_nimmt+2].strip()
+					card_name = line_split[pos_nimmt+2].strip().replace(".", "")
 				amount = int(amount_str.replace("einen", "1").replace("eine", "1").replace("ein", "1"))
-				# print("%s zieht %d %s" % (players_name, amount, card_name))
+				# print("%s entsorgt %d %s" % (players_name, amount, card_name))
 
 				if card_name in players[players_name].cards:
-						# print("Kartentyp bereits gefunden")
-						players[players_name].cards[card_name] = players[players_name].cards[card_name] - amount
-						players[players_name].cards_log_diff[(turn, card_name)] = -amount
+					# print("Kartentyp bereits gefunden")
+					players[players_name].cards[card_name] = players[players_name].cards[card_name] - amount
+					players[players_name].cards_log_diff[(turn, card_name)] = -amount
+					
+					# kosmetische Diagrammgründe
+					if players[players_name].cards[card_name] == 0:
+						players[players_name].cards.pop(card_name)
+						players[players_name].cards_log_cum[(turn, card_name)] = 1
+					else:
+						players[players_name].cards_log_cum[(turn, card_name)] = players[players_name].cards[card_name]
+
+			if "legt" in line and "zurück" in line and not "Marker" in line:
+				if line.strip().endswith("."):
+					line_str = line[:-1]
+				else:
+					line_str = line
+				line_split = line_str.split(" ")
+				pos_nimmt = line_split.index("legt")
+				amount_str = line_split[pos_nimmt+1]
+				players_name = line_split[0]
+
+				# names with two parts
+				if (line_split.index("zurück")) - (pos_nimmt+2) > 1:
+					card_name = line_split[pos_nimmt+2] + " " + line_split[pos_nimmt+3].strip().replace(".", "")
+				else:
+					card_name = line_split[pos_nimmt+2].strip().replace(".", "")
+				amount = int(amount_str.replace("einen", "1").replace("eine", "1").replace("ein", "1"))
+				# print("%s legt %d %s zurück" % (players_name, amount, card_name))
+
+				if card_name == "Pferd":
+					card_name = "Pferde"
+
+				if card_name in players[players_name].cards:
+					# print("Kartentyp bereits gefunden")
+					players[players_name].cards[card_name] = players[players_name].cards[card_name] - amount
+					players[players_name].cards_log_diff[(turn, card_name)] = -amount
+					
+					# kosmetische Diagrammgründe
+					if players[players_name].cards[card_name] == 0:
+						# players[players_name].cards.pop(card_name)
+						players[players_name].cards_log_cum[(turn, card_name)] = 1
+					else:
 						players[players_name].cards_log_cum[(turn, card_name)] = players[players_name].cards[card_name]
 
 		for player in players.values():
-			print(player.name)
-			print(player.cards)
+			for card_name, amount in player.cards.items():
+				player.cards_log_cum[(turn, card_name)] = player.cards[card_name]
 			
 	with open("results_diff.csv", mode='w') as evalTable:
 		evalTable.write("Name,Zug,Karte,Anzahl\n")
